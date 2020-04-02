f33ba85f93e6058a2cb0e46e833ba97661e60e52
piaand <pia.m.andersin@gmail.com>
2020-04-02 13:30:49 +0300

    added exception: integer prints nothing when precision is 0 and value 0. Also signed char conversion added

--- src/print_integer.c
+++ src/print_integer.c
@@ -6,17 +6,19 @@
 /*   By: piaandersin <piaandersin@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/31 09:17:17 by piaandersin       #+#    #+#             */
-/*   Updated: 2020/04/02 11:22:24 by piaandersin      ###   ########.fr       */
+/*   Updated: 2020/04/02 13:25:52 by piaandersin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static char	*convert_number(long long int nb, int base) 
+static char	*convert_number(long long int nb, int base, t_tag **format) 
 {
 	char *print_int;
 
-	if(!(print_int = ft_itoa_base(nb, base)))
+	if ((*format)->has_value[PRECISION_ON] == '1' && (*format)->precision == 0 && nb == 0)
+		print_int = "";
+	else if(!(print_int = ft_itoa_base(nb, base)))
 		ft_error("itoa returned a NULL pointer.");
 	return (print_int);
 }
@@ -28,14 +30,16 @@ static char *control_length(t_tag **format, long long i)
 	if ((*format)->has_value[LENGTH_ON] == '1')
 	{
 		if (ft_strequ((*format)->length, "0h"))
-			print_int = convert_number((short int)i, 10);
+			print_int = convert_number((short int)i, 10, format);
 		else if (ft_strequ((*format)->length, "0l"))
-			print_int = convert_number((long int)i, 10);
+			print_int = convert_number((long int)i, 10, format);
+		else if (ft_strequ((*format)->length, "hh"))
+			print_int = convert_number((signed char)i, 10, format);
 		else
-			print_int = convert_number(i, 10);
+			print_int = convert_number(i, 10, format);
 	}
 	else
-		print_int = convert_number((int)i, 10);
+		print_int = convert_number((int)i, 10, format);
 	return (print_int);
 }
 
